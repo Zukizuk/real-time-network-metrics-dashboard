a6f98f9a8bd0ffd496b7490e6220174f2814677c
Sanni Marzuk <sannimarzuk@gmail.com>
2025-05-13 09:44:22 +0000

    Provision Kinesis Data Stream

---
+++ module/kinesis/main.tf
@@ -0,0 +1,9 @@
+resource "aws_kinesis_stream" "my-stream" {
+  name                = "metric-stream"
+  shard_count         = 1
+  retention_period    = 24
+  shard_level_metrics = ["IncomingBytes", "OutgoingBytes"]
+  stream_mode_details {
+    stream_mode = "PROVISIONED"
+  }
+}
